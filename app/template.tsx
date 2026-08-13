a09b81a6e9ad0ae2094c30672476b7412c3a8124
menorbillabong <menor.billabong@gmail.com>
2026-08-13 17:48:19 -0300

    Mount monthly goal persistence sync

--- app/template.tsx
+++ app/template.tsx
@@ -1,6 +1,7 @@
 import BulkReviewInjector from './bulk-review-injector';
 import SiteUpgrades from './site-upgrades';
 import PostSortEnhancer from './post-sort-enhancer';
+import SettingsPersistenceSync from './settings-persistence-sync';
 import './site-upgrades.css';
 import './full-settings-v2.css';
-export default function Template({children}:{children:React.ReactNode}){return <>{children}<BulkReviewInjector/><SiteUpgrades/><PostSortEnhancer/></>}
+export default function Template({children}:{children:React.ReactNode}){return <>{children}<BulkReviewInjector/><SiteUpgrades/><PostSortEnhancer/><SettingsPersistenceSync/></>}
