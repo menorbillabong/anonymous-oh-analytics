79e8737f3bac68c459b564c5a22b57d0a89a4be9
menorbillabong <menor.billabong@gmail.com>
2026-08-13 20:03:34 -0300

    Mount database-backed refresh timer

--- app/template.tsx
+++ app/template.tsx
@@ -2,7 +2,8 @@ import BulkReviewInjector from './bulk-review-injector';
 import SiteUpgrades from './site-upgrades';
 import PostSortEnhancer from './post-sort-enhancer';
 import SettingsPersistenceSync from './settings-persistence-sync';
+import DatabaseRefreshTimer from './database-refresh-timer';
 import './site-upgrades.css';
 import './full-settings-v2.css';
 import './color-customization.css';
-export default function Template({children}:{children:React.ReactNode}){return <>{children}<BulkReviewInjector/><SiteUpgrades/><PostSortEnhancer/><SettingsPersistenceSync/></>}
+export default function Template({children}:{children:React.ReactNode}){return <>{children}<BulkReviewInjector/><SiteUpgrades/><PostSortEnhancer/><SettingsPersistenceSync/><DatabaseRefreshTimer/></>}
