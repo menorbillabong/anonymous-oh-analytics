4d61f989fe5a3051ec9eac80c91eae43ab1cdbb5
menorbillabong <menor.billabong@gmail.com>
2026-08-13 17:32:47 -0300

    Mount site upgrades globally

--- app/template.tsx
+++ app/template.tsx
@@ -1,2 +1,4 @@
 import BulkReviewInjector from './bulk-review-injector';
-export default function Template({children}:{children:React.ReactNode}){return <>{children}<BulkReviewInjector/></>}
+import SiteUpgrades from './site-upgrades';
+import './site-upgrades.css';
+export default function Template({children}:{children:React.ReactNode}){return <>{children}<BulkReviewInjector/><SiteUpgrades/></>}
