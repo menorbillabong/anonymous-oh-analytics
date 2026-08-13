e5d56b1eae98a2fd66ca7dec5116f664d062ac00
menorbillabong <menor.billabong@gmail.com>
2026-08-13 18:00:37 -0300

    Load color customization styles

--- app/template.tsx
+++ app/template.tsx
@@ -4,4 +4,5 @@ import PostSortEnhancer from './post-sort-enhancer';
 import SettingsPersistenceSync from './settings-persistence-sync';
 import './site-upgrades.css';
 import './full-settings-v2.css';
+import './color-customization.css';
 export default function Template({children}:{children:React.ReactNode}){return <>{children}<BulkReviewInjector/><SiteUpgrades/><PostSortEnhancer/><SettingsPersistenceSync/></>}
