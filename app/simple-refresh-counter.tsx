642fc917562ad54c87d6bf42913ca393f6aa6683
menorbillabong <menor.billabong@gmail.com>
2026-08-13 20:25:05 -0300

    Make refresh countdown visible with legacy timer CSS present

--- app/simple-refresh-counter.tsx
+++ app/simple-refresh-counter.tsx
@@ -1,1 +1,14 @@
-export {default} from './refresh-countdown';
+'use client';
+import {useEffect} from 'react';
+import RefreshCountdown from './refresh-countdown';
+
+export default function SimpleRefreshCounter(){
+ useEffect(()=>{
+  const reveal=()=>document.querySelectorAll('.refresh-countdown-host [title="Tempo restante para a próxima atualização"]').forEach(el=>el.removeAttribute('title'));
+  reveal();
+  const observer=new MutationObserver(reveal);
+  observer.observe(document.body,{childList:true,subtree:true,attributes:true,attributeFilter:['title']});
+  return()=>observer.disconnect();
+ },[]);
+ return <RefreshCountdown/>;
+}
