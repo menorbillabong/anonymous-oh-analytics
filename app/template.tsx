bdf8715069bf97341aa5ba4df0e464a8e93c3e41
menorbillabong <menor.billabong@gmail.com>
2026-08-13 20:10:21 -0300

    Remove current refresh counter from interface

--- app/template.tsx
+++ app/template.tsx
@@ -2,8 +2,7 @@ import BulkReviewInjector from './bulk-review-injector';
 import SiteUpgrades from './site-upgrades';
 import PostSortEnhancer from './post-sort-enhancer';
 import SettingsPersistenceSync from './settings-persistence-sync';
-import DatabaseRefreshTimer from './database-refresh-timer';
 import './site-upgrades.css';
 import './full-settings-v2.css';
 import './color-customization.css';
-export default function Template({children}:{children:React.ReactNode}){return <>{children}<BulkReviewInjector/><SiteUpgrades/><PostSortEnhancer/><SettingsPersistenceSync/><DatabaseRefreshTimer/></>}
+export default function Template({children}:{children:React.ReactNode}){return <>{children}<BulkReviewInjector/><SiteUpgrades/><PostSortEnhancer/><SettingsPersistenceSync/></>}
